2a9250dd02899bcef6831de38556dd1d494f8efa
Jamie Sherman <jamies@alleninstitute.org>
2018-08-10 13:58:12 -0700

    min entropy class for applying learned model to db dataset

--- MinEntropy.py
+++ MinEntropy.py
@@ -1,14 +1,12 @@
 import os, sys
 
 sys.path.append("src")
-from train_model import train_model
-from model_analysis import model_analysis, torch_confusion_matrix, plot_confusion_matrix
-from plot_images import torch_to_PIL_single_image, ims_labels_to_grid, ims_preds_to_grid, ims_labels_preds_to_grid
 
 import numpy as np
 import pandas as pd
 import json
 import datasetdatabase as dsdb
+import datetime
 
 import torch
 from torch import nn
@@ -22,18 +20,8 @@ from pytorch_learning_tools.data_providers.DataframeDataset import DatasetSingle
 from pytorch_learning_tools.utils.data_utils import classes_and_weights
 
 import collections
-from sqlalchemy import create_engine
 
-import torch._utils
-try:
-    torch._utils._rebuild_tensor_v2
-except AttributeError:
-    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
-        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
-        tensor.requires_grad = requires_grad
-        tensor._backward_hooks = backward_hooks
-        return tensor
-    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+
 
 class MitosisClassifier(object):
     """This is a script to automate the running of the mitotic program
@@ -45,7 +33,7 @@ class MitosisClassifier(object):
 
     def __init__(self, base_path, f_label='mitosis_label'):
         self.GPU_ID = 1
-        self.BATCH_SIZE = 64
+        self.BATCH_SIZE = 64  # 64
         self.base_path = base_path
         self.greg_data = '/allen/aics/modeling/gregj/results/ipp/ipp_17_12_03/'
         self.csv_input = 'input_data_files/mito_annotations_all.csv'  #
@@ -75,13 +63,16 @@ class MitosisClassifier(object):
     def class_names(self):
         return self.m_class_names
 
+    def get_dpX(self):
+        return self.dpX
+
     def read_and_filter_input(self, f_label='mitosis_label'):
         dbConnectionInfo = json.load(open('/allen/aics/modeling/jamies/projects/dbconnect/configs.json', 'r'))
         mngr = dsdb.ConnectionManager(user="jamies")
         mngr.add_connections(dbConnectionInfo)
         prod = mngr.connect('prod')
         dfio = prod.get_dataset(1)
-        dfio['save_flat_proj_reg_path'] = '/allen/aics/modeling/PIPELINE/2018-07-23-17:20:57/' + dfio['save_flat_proj_reg_path']
+        #dfio['save_flat_proj_reg_path'] = '/allen/aics/modeling/PIPELINE/2018-07-23-17:20:57/' + dfio['save_flat_proj_reg_path']
         dfio['target_numeric'] = -1
         df = dfio.copy(deep=True)
         return df
@@ -111,7 +102,7 @@ class MitosisClassifier(object):
         return dp
 
     def ofname(self, b_name, f_ext):
-        fname = "{0}_{1}.{2}".format(b_name, str(self.iter_n).zfill(2), f_ext)
+        fname = "{0}_{1}.{2}".format(datetime.datetime.now().isoformat(timespec='minutes'), b_name, f_ext)
         return os.path.join(self.base_path, fname)
 
     def generate_class_weights(self):
@@ -137,60 +128,36 @@ class MitosisClassifier(object):
         model = model.cuda(self.GPU_ID)
         model.eval()
         return model
+    #
+    # def load_models_xyz(self, mPathX, mPathY, mPathZ):
+    #     self.modelX = self.load_model(mPathX)
+    #     self.modelY = self.load_model(mPathY)
+    #     self.modelZ = self.load_model(mPathZ)
 
-    def load_models_xyz(self, mPathX, mPathY, mPathZ):
-        self.modelX = self.load_model(mPathX)
-        self.modelY = self.load_model(mPathY)
-        self.modelZ = self.load_model(mPathZ)
-
-
-    # def oapply_models(self):
-    #     mito_labels = {k: {'x_pred_labels': [], 'x_pred_entropy': [], 'x_pred_uid': [], 'x_probability': [],
-    #                        'y_pred_labels': [], 'y_pred_entropy': [], 'y_pred_uid': [], 'y_probability': [],
-    #                        'z_pred_labels': [], 'z_pred_entropy': [], 'z_pred_uid': [], 'z_probability': [],
-    #                        'pred_labels': []
-    #                        } for k in self.dpX.dataloaders.keys()}
-    #
-    #     for phase in self.dpX.dataloaders.keys():
-    #         for i in range(len(self.dpX.dataloaders[phase])):
-    #             ans_x = self.apply_itt(i, self.dpX.dataloaders[phase], self.modelX)
-    #             ans_y = self.apply_itt(i, self.dpY.dataloaders[phase], self.modelY)
-    #             ans_z = self.apply_itt(i, self.dpZ.dataloaders[phase], self.modelZ)
-    #
-    #             mito_labels[phase]['x_pred_labels'] += ans_x.pred_labels
-    #             mito_labels[phase]['x_pred_entropy'] += ans_x.pred_entropy
-    #             mito_labels[phase]['x_pred_uid'] += ans_x.uid
-    #             mito_labels[phase]['x_probability'] += ans_x.probs
-    #
-    #             mito_labels[phase]['y_pred_labels'] += ans_y.pred_labels
-    #             mito_labels[phase]['y_pred_entropy'] += ans_y.pred_entropy
-    #             mito_labels[phase]['y_pred_uid'] += ans_y.uid
-    #             mito_labels[phase]['y_probability'] += ans_y.probs
-    #
-    #             mito_labels[phase]['z_pred_labels'] += ans_z.pred_labels
-    #             mito_labels[phase]['z_pred_entropy'] += ans_z.pred_entropy
-    #             mito_labels[phase]['z_pred_uid'] += ans_z.uid
-    #             mito_labels[phase]['z_probability'] += ans_z.probs
-    #
-    #             mito_labels[phase]['pred_labels'] += self.min_entropy(ans_x, ans_y, ans_z)
-
-    def apply_models(self):
+    def apply_models(self, xpath, ypath, zpath):
         mito_labels = {k: {'x_pred_labels': [], 'x_pred_entropy': [], 'x_pred_uid': [], 'x_probability': [],
                            'y_pred_labels': [], 'y_pred_entropy': [], 'y_pred_uid': [], 'y_probability': [],
                            'z_pred_labels': [], 'z_pred_entropy': [], 'z_pred_uid': [], 'z_probability': [],
                            'pred_labels': []
                            } for k in self.dpX.dataloaders.keys()}
-        self.apply_single_model(self.dpX, self.modelX, mito_labels,
-                                'x_pred_labels', 'x_pred_entropy', 'x_pred_uid', 'x_probability')
-        self.apply_single_model(self.dpY, self.modelY, mito_labels,
-                                'y_pred_labels', 'y_pred_entropy', 'y_pred_uid', 'y_probability')
-        self.apply_single_model(self.dpZ, self.modelZ, mito_labels,
-                                'z_pred_labels', 'z_pred_entropy', 'z_pred_uid', 'z_probability')
+
+        self.apply_single_model(self.dpX, mito_labels,
+                                'x_pred_labels', 'x_pred_entropy', 'x_pred_uid', 'x_probability', xpath)
+        print("XModel done")
+
+        self.apply_single_model(self.dpY, mito_labels,
+                                'y_pred_labels', 'y_pred_entropy', 'y_pred_uid', 'y_probability', ypath)
+        print("YModel done")
+
+        self.apply_single_model(self.dpZ, mito_labels,
+                                'z_pred_labels', 'z_pred_entropy', 'z_pred_uid', 'z_probability', zpath)
+        print("ZModel done")
 
         self.apply_min_entropy(mito_labels)
         self.mito_labels = mito_labels
 
-    def apply_single_model(self, dp_h, model, mlabels, h_pred_labels, h_pred_entropy, h_pred_uid, h_probability):
+    def apply_single_model(self, dp_h, mlabels, h_pred_labels, h_pred_entropy, h_pred_uid, h_probability, mPath):
+        model = self.load_model(mPath)
         for phase in dp_h.dataloaders.keys():
             for i, mb in enumerate(dp_h.dataloaders[phase]):
                 x = mb['image']
@@ -208,37 +175,21 @@ class MitosisClassifier(object):
                 mlabels[phase][h_pred_labels] += pred_labels
                 mlabels[phase][h_pred_entropy] += pred_entropy
                 mlabels[phase][h_pred_uid] += u
-                mlabels[phase][h_probability] += list(probs)
+                mlabels[phase][h_probability] += list(probs.data.cpu())
 
     def apply_min_entropy(self, mlabels):
         for phase in self.dpX.dataloaders.keys():
             for i in range(len(mlabels[phase]['x_pred_labels'])):
-                mlabels[phase]['pred_labels'] += self.min_entropy(mlabels[phase]['x_pred_labels'][i],
-                                                                  mlabels[phase]['x_pred_entropy'][i],
-                                                                  mlabels[phase]['y_pred_labels'][i],
-                                                                  mlabels[phase]['y_pred_entropy'][i],
-                                                                  mlabels[phase]['z_pred_labels'][i],
-                                                                  mlabels[phase]['z_pred_entropy'][i]
-                                                                  )
-
-    # def apply_itt(self, i, dloader, model):
-    #     itt = dloader[i]
-    #     x = itt['image']
-    #     u = itt['uniqueID']
-    #
-    #     y_hat_pred = model(Variable(x).cuda(self.GPU_ID))
-    #     _, y_hat = y_hat_pred.max(1)
-    #
-    #     probs = F.softmax(y_hat_pred, dim=1)
-    #     entropy = -torch.sum(probs * torch.log(probs), dim=1)
-    #
-    #     pred_labels = list(y_hat.data.cpu().numpy())
-    #     pred_entropy = list(entropy.data.cpu().numpy())
-    #     PredResults = collections.namedtuple('PredResults', ['pred_labels', 'pred_entropy', 'uid', 'probs'])
-    #     ans = PredResults(pred_labels, pred_entropy, u, probs)
-    #     return ans
+                mlabels[phase]['pred_labels'].append(self.min_entropy(mlabels[phase]['x_pred_labels'][i],
+                                                                      mlabels[phase]['x_pred_entropy'][i],
+                                                                      mlabels[phase]['y_pred_labels'][i],
+                                                                      mlabels[phase]['y_pred_entropy'][i],
+                                                                      mlabels[phase]['z_pred_labels'][i],
+                                                                      mlabels[phase]['z_pred_entropy'][i])
+                                                     )
 
     def save_out(self):
+        uidKey = self.dpX.opts['uniqueID']
         df_pred = pd.DataFrame({'MitosisLabelPredicted': self.mito_labels['all']['pred_labels'],
                                 'X_MitoLabel': self.mito_labels['all']['x_pred_labels'],
                                 'Y_MitoLabel': self.mito_labels['all']['y_pred_labels'],
@@ -249,9 +200,9 @@ class MitosisClassifier(object):
                                 'X_MitoProb': self.mito_labels['all']['x_probability'],
                                 'Y_MitoProb': self.mito_labels['all']['y_probability'],
                                 'Z_MitoProb': self.mito_labels['all']['z_probability'],
-                                self.dp.opts['uniqueID']: self.mito_labels['all']['x_pred_uid']})
-
-        df_out = pd.merge(self.dp.dfs['all'], df_pred, how='inner', on=self.dp.opts['uniqueID'])
+                                uidKey: self.mito_labels['all']['x_pred_uid']})
+        print("df_pred: ", df_pred.shape)
+        df_out = pd.merge(self.dpX.dfs['all'], df_pred, how='inner', on=uidKey)
         # df_out = df_out.drop(columns='target_numeric')
         fname = self.ofname('mitotic_predictions_on_unannotated_cells', 'csv')
         df_out.to_csv(fname)
@@ -263,8 +214,8 @@ class MitosisClassifier(object):
         df = self.read_and_filter_input()
         self.create_data_providers_xyz(df)
         print("ready to run.")
-        self.load_models_xyz(xpath, ypath, zpath)
-        self.apply_models()
+        #self.load_models_xyz(xpath, ypath, zpath)
+        self.apply_models(xpath, ypath, zpath)
         self.save_out()
         print("finished.")
 
@@ -370,3 +321,17 @@ class MitosisClassifier(object):
                  blank,
                  x[2, y1s:(w + y1s), x2s:(w + x2s)]
                  ]))])
+
+
+# This is a hack to bootstrap models trained on aics001 into newer version of pytorch
+#
+# import torch._utils
+# try:
+#     torch._utils._rebuild_tensor_v2
+# except AttributeError:
+#     def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+#         tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+#         tensor.requires_grad = requires_grad
+#         tensor._backward_hooks = backward_hooks
+#         return tensor
+#     torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
