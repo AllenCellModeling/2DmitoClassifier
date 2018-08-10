5eb5515e36bb30577ab6b450dc79682f133435f4
Jamie Sherman <jamies@alleninstitute.org>
2018-08-10 15:47:20 -0700

    cleaned up ingestion

--- LoadPTModelsToDb.py
+++ LoadPTModelsToDb.py
@@ -8,10 +8,15 @@ mngr = dsdb.ConnectionManager(user="jamies")
 mngr.add_connections(dbConnectionInfo)
 prod = mngr.connect('prod')
 
-model_files = []
-model_files.append({'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/X/saved_model_10E_01.pt', 'axis':'X', 'param_order': 0})
-model_files.append({'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/Y/saved_model_10E_03.pt', 'axis':'Y', 'param_order': 1})
-model_files.append({'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/Z/saved_model_10E_09.pt', 'axis':'Z', 'param_order': 2})
+model_files = [
+    {'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/X/saved_model_10E_01.pt',
+     'axis': 'X', 'param_order': 0},
+    {'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/Y/saved_model_10E_03.pt',
+        'axis': 'Y', 'param_order': 1},
+    {'pytorch_model': '/allen/aics/modeling/jamies/projects/three_channel/XYZ3/Z/saved_model_10E_09.pt',
+     'axis': 'Z', 'param_order': 2}
+]
+
 df = pd.DataFrame(model_files)
 
 ds_info = prod.upload_dataset(dataset=df,
